dd833d0521081f5b3ba460fff30d12989c968f07
Egor Karpenko <92591674+Egor-Karpenko@users.noreply.github.com>
2021-11-18 17:54:58 +0300

    add files

---
+++ js/main.js
@@ -0,0 +1,31 @@
+async function getRasponce() {
+    let responce = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let content = await responce.json()
+    content = content;
+
+    let list = document.querySelector('.posts');
+    let key;
+
+
+    for (key in content) {
+
+        list.innerHTML += `
+        <li class = "post" style="border: 1px solid #000000;""font-size : 30px;" >
+                <h4 >${content[key].title}</h4>
+                <img src="./Без названия.jfif" width="200" > 
+                
+                <button id="AlertButtton">alert</button>
+                <button id = "DeleteButton">delete</button>   
+                
+                                               
+        </li>
+        `
+    }
+    document.querySelector("#AlertButtton").onclick = function() {
+        alert(`${content[key].title}`);
+    }
+
+
+
+}
+getRasponce()
